6e7e758eebde4dda7e89e82242b4a4b984b3d38e
AtharvChandratre <atharvchandratre@gmail.com>
2021-11-13 04:40:19 +0000

    Deploying to gh-pages from  @ f6c40330d612e3f177250f4033e6df74ab796145 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.d288643377661bb67afee87ccd667701.js"
+  "/portfolio/precache-manifest.96ae6f2dcc3c5e27479aef9b62aa645c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
